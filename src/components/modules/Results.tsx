943b53c48e9c88469b78ad54639217513bb90a12
trsk269 <t.r.s.kumar6054@gmail.com>
2026-01-05 08:08:01 +0530

    /note/add edit

--- src/components/modules/Results.tsx
+++ src/components/modules/Results.tsx
@@ -22,7 +22,7 @@ export default function Result() {
     return (
       <div className="w-full h-full flex items-center justify-center">
         <Link
-          href="/add"
+          href="/note/add"
           className="border border-gray-400 text-gray-400 px-4 py-2 rounded-md"
         >
           + Add your first note
